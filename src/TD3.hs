5c42f2d16429ebf90e75da05a74093b6a617ab30
Yannick Uhlmann <augustunderground@protonmail.com>
2022-03-17 10:35:35 +0100

    added TD3

--- src/TD3.hs
+++ src/TD3.hs
@@ -1,6 +1,334 @@
+{-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 module TD3 ( algorithm
+           , Agent
+           , makeAgent
+           , saveAgent
+           , π
+           , q
+           , q'
+           , addNoise
+           , train
+           -- , play
            ) where
 
 import Lib
+import PER
+import TD3.Defaults
 
-algorithm = "td3"
+import Control.Monad
+import GHC.Generics
+import qualified Torch    as T
+import qualified Torch.NN as NN
+
+------------------------------------------------------------------------------
+-- Neural Networks
+------------------------------------------------------------------------------
+
+-- | Actor Network Specification
+data ActorNetSpec = ActorNetSpec { pObsDim :: Int, pActDim :: Int }
+    deriving (Show, Eq)
+
+-- | Critic Network Specification
+data CriticNetSpec = CriticNetSpec { qObsDim :: Int, qActDim :: Int }
+    deriving (Show, Eq)
+
+-- | Actor Network Architecture
+data ActorNet = ActorNet { pLayer0 :: T.Linear
+                         , pLayer1 :: T.Linear
+                         , pLayer2 :: T.Linear }
+    deriving (Generic, Show, T.Parameterized)
+
+-- | Critic Network Architecture
+data CriticNet = CriticNet { qLayer0 :: T.Linear
+                           , qLayer1 :: T.Linear
+                           , qLayer2 :: T.Linear }
+    deriving (Generic, Show, T.Parameterized)
+
+-- | Actor Network Weight initialization
+instance T.Randomizable ActorNetSpec ActorNet where
+    sample ActorNetSpec{..} = ActorNet <$> ( T.sample (T.LinearSpec pObsDim 400) 
+                                             >>= weightInit' )
+                                       <*> ( T.sample (T.LinearSpec 400     300)
+                                             >>= weightInit' )
+                                       <*> ( T.sample (T.LinearSpec 300 pActDim)
+                                             >>= weightInit wInit )
+
+-- | Critic Network Weight initialization
+instance T.Randomizable CriticNetSpec CriticNet where
+    sample CriticNetSpec{..} = CriticNet <$> ( T.sample (T.LinearSpec dim 400) 
+                                               >>= weightInit' )
+                                         <*> ( T.sample (T.LinearSpec 400 300) 
+                                               >>= weightInit' )
+                                         <*> ( T.sample (T.LinearSpec 300 1) 
+                                               >>= weightInit' )
+        where dim = qObsDim + qActDim
+
+-- | Actor Network Forward Pass
+π :: ActorNet -> T.Tensor -> T.Tensor
+π ActorNet{..} !o = a
+  where
+    !a = T.tanh . T.linear pLayer2 
+       . T.relu . T.linear pLayer1 
+       . T.relu . T.linear pLayer0 
+       $ o
+
+-- | Critic Network Forward Pass
+q :: CriticNet -> T.Tensor -> T.Tensor -> T.Tensor
+q CriticNet{..} !o !a = v
+  where 
+    !x = T.cat (T.Dim $ -1) [o,a]
+    !v = T.linear qLayer2 . T.relu
+       . T.linear qLayer1 . T.relu
+       . T.linear qLayer0 $ x
+
+-- | Convenience Function
+q' :: CriticNet -> CriticNet -> T.Tensor -> T.Tensor -> T.Tensor
+q' !c1 !c2 !s !a = v
+  where
+    !q1 = q c1 s a
+    !q2 = q c2 s a
+    !v  = fst . T.minDim (T.Dim 1) T.KeepDim $ T.cat (T.Dim 1) [q1, q2]
+
+------------------------------------------------------------------------------
+-- TD3 Agent
+------------------------------------------------------------------------------
+
+-- | TD3 Agent
+data Agent = Agent { φ       :: ActorNet  
+                   , φ'      :: ActorNet  
+                   , θ1      :: CriticNet  
+                   , θ2      :: CriticNet  
+                   , θ1'     :: CriticNet  
+                   , θ2'     :: CriticNet  
+                   , φOptim  :: T.Adam
+                   , θ1Optim :: T.Adam
+                   , θ2Optim :: T.Adam
+                   } deriving (Generic, Show)
+
+-- | Agent constructor
+makeAgent :: Int -> Int -> IO Agent
+makeAgent obsDim actDim = do
+    φOnline   <- toFloatGPU <$> T.sample (ActorNetSpec obsDim actDim)
+    φTarget'  <- toFloatGPU <$> T.sample (ActorNetSpec obsDim actDim)
+    θ1Online  <- toFloatGPU <$> T.sample (CriticNetSpec obsDim actDim)
+    θ2Online  <- toFloatGPU <$> T.sample (CriticNetSpec obsDim actDim)
+    θ1Target' <- toFloatGPU <$> T.sample (CriticNetSpec obsDim actDim)
+    θ2Target' <- toFloatGPU <$> T.sample (CriticNetSpec obsDim actDim)
+
+    let φTarget  = copySync φTarget'  φOnline
+        θ1Target = copySync θ1Target' θ1Online
+        θ2Target = copySync θ2Target' θ2Online
+    
+
+    let φOpt  = T.mkAdam 0 β1 β2 (NN.flattenParameters φOnline)
+        θ1Opt = T.mkAdam 0 β1 β2 (NN.flattenParameters θ1Online)
+        θ2Opt = T.mkAdam 0 β1 β2 (NN.flattenParameters θ2Online)
+
+    pure $ Agent φOnline φTarget θ1Online θ2Online θ1Target θ2Target
+                 φOpt            θ1Opt    θ2Opt                      
+
+-- | Save an Agent to Disk
+saveAgent :: Agent -> String -> IO ()
+saveAgent Agent{..} path = head $ zipWith T.saveParams 
+                                [q1o, q2o, q1t, q2t, ao, at] 
+                                [pq1o, pq2o, pq1t, pq2t, pao, pat]
+  where
+    q1o  = T.toDependent <$> T.flattenParameters θ1
+    q2o  = T.toDependent <$> T.flattenParameters θ2
+    q1t  = T.toDependent <$> T.flattenParameters θ1'
+    q2t  = T.toDependent <$> T.flattenParameters θ2'
+    ao   = T.toDependent <$> T.flattenParameters φ
+    at   = T.toDependent <$> T.flattenParameters φ'
+    pq1o = path ++ "/q1o.pt"
+    pq2o = path ++ "/q2o.pt"
+    pq1t = path ++ "/q1t.pt"
+    pq2t = path ++ "/q2t.pt"
+    pao  = path ++ "/actor.pt"
+    pat  = path ++ "/actor.pt"
+
+---- | Load an Actor Net
+--loadActor :: String -> Int -> Int -> IO ActorNet
+
+---- | Load an Critic Net
+--loadCritic :: String -> Int -> Int -> IO CriticNet
+
+---- | Load an Agent
+--loadCritic :: String -> IO Agent
+
+addNoise :: Int -> T.Tensor -> IO T.Tensor
+addNoise t action = do
+    ε <- (σ' *) <$> normal' [l]
+    let action' = T.clamp (- 1.0) 1.0 (action + ε)
+    pure action'
+  where
+    l  = T.shape action !! 1
+    d' = realToFrac decayPeriod
+    t' = realToFrac t
+    m  = min 1.0 (t' / d')
+    σ' = toTensor $ σMin - (σMax - σMin) * m
+
+------------------------------------------------------------------------------
+-- Training
+------------------------------------------------------------------------------
+
+-- | Policy Update Step
+updateStep :: Int -> Int -> Int -> Agent -> ReplayBuffer -> IO Agent
+updateStep _ _ 0 agent _ = pure agent
+updateStep episode iteration epoch Agent{..} buffer@ReplayBuffer{..} = do
+    ε <- normal μ' σ'
+    let a' = π φ' s' + ε
+    y <- T.detach $ r + γ * q' θ1' θ2' s' a'
+
+    let v1 = q θ1 s a
+        v2 = q θ2 s a
+        jQ1 = T.mseLoss v1 y
+        jQ2 = T.mseLoss v2 y
+
+    when (verbose && iteration `elem` [0,10 .. numIterations]) do
+        putStrLn $ "\tΘ1  Loss:\t" ++ show jQ1
+        putStrLn $ "\tΘ2  Loss:\t" ++ show jQ2
+    writeLoss episode iteration "Q1" (T.asValue jQ1 :: Float)
+    writeLoss episode iteration "Q2" (T.asValue jQ2 :: Float)
+
+    (θ1Online', θ1Optim') <- T.runStep θ1 θ1Optim jQ1 ηθ
+    (θ2Online', θ2Optim') <- T.runStep θ2 θ1Optim jQ2 ηθ
+
+    let updateActor :: IO (ActorNet, T.Adam)
+        updateActor = do
+            let jφ = - (T.mean v)
+            when (verbose && iteration `elem` [0,10 .. numIterations]) do
+                putStrLn $ "\tφ  Loss:\t" ++ show jφ
+            writeLoss episode iteration "A" (T.asValue jφ :: Float)
+            T.runStep φ φOptim jφ ηφ
+          where
+            a'' = π φ s
+            v = q θ1 a'' v
+        syncTargets :: IO (ActorNet, CriticNet, CriticNet)
+        syncTargets = do
+            φTarget'  <- softSync τ φ'  φ
+            θ1Target' <- softSync τ θ1' θ1 
+            θ2Target' <- softSync τ θ2' θ2 
+            pure (φTarget', θ1Target', θ2Target')
+
+    (φOnline', φOptim') <- if iteration `elem` [0,d .. numIterations]
+                              then updateActor
+                              else pure (φ, φOptim)
+
+    (φTarget', θ1Target', θ2Target') <- if iteration `elem` [0,d .. numIterations]
+                                           then syncTargets
+                                           else pure (φ', θ1', θ2')
+
+
+    let agent' = Agent φOnline' φTarget' θ1Online' θ2Online' θ1Target' θ2Target' 
+                       φOptim'           θ1Optim'  θ2Optim'
+
+    updateStep episode iteration epoch' agent' buffer
+  where
+    μ'      = T.zerosLike actions
+    σ'      = T.onesLike μ' * σ
+    epoch'  = epoch - 1
+    s       = states
+    a       = actions
+    r       = rewards
+    s'      = states'
+
+-- | Perform Policy Update Steps
+updatePolicy :: Int -> Int -> Agent -> ReplayBuffer -> Int -> IO Agent
+updatePolicy episode iteration !agent !buffer epochs = do
+    memories <- bufferRandomSample batchSize buffer
+    updateStep episode iteration epochs agent memories
+
+-- | Evaluate Policy
+evaluatePolicy :: Int -> Int -> Int -> Agent -> HymURL -> T.Tensor 
+               -> ReplayBuffer -> T.Tensor -> IO (ReplayBuffer, T.Tensor, T.Tensor)
+evaluatePolicy _ _ 0 _ _ obs buffer total = pure (buffer, obs, total)
+evaluatePolicy episode iteration step agent@Agent{..} envUrl obs buffer total = do
+    actions <- if p < warmupPeriode
+                  then randomActionPool envUrl
+                  else addNoise iteration (π φ obs)
+
+    (obs'', rewards, dones, infos) <- stepPool envUrl actions
+
+    let keys    = head infos
+        total'  = T.cat (T.Dim 0) [total, rewards]
+    
+    obs' <- if T.any dones 
+               then flip processGace keys <$> resetPool' envUrl dones
+               else pure $ processGace obs'' keys
+
+    let buffer' = bufferPush bufferSize buffer obs actions rewards obs' dones
+
+    writeReward' episode iteration rewards
+
+    when (verbose && iteration `elem` [0,10 .. numIterations]) do
+        putStrLn $ "\tAverage Reward:\t" ++ show (T.mean rewards)
+
+    when (verbose && T.any dones) do
+        let de = T.squeezeAll . T.nonzero $ dones
+        putStrLn $ "Environments " ++ " done after " ++ show iteration 
+                ++ " iterations, resetting:\n\t" ++ show de
+
+    evaluatePolicy episode iteration step' agent envUrl obs' buffer' total'
+  where
+    p       = iteration * numEnvs
+    step'   = step - 1
+
+-- | Run Twin Delayed Deep Deterministic Policy Gradient Training
+runAlgorithm :: Int -> Int -> Agent -> HymURL -> Bool -> ReplayBuffer 
+             -> T.Tensor -> T.Tensor -> IO Agent
+runAlgorithm episode iteration agent _ True _ _ reward = do
+    putStrLn $ "Episode " ++ show episode ++ " done after " ++ show iteration 
+            ++ " iterations, with a total reward of " ++ show reward'
+    pure agent
+  where
+    reward' = T.asValue . T.sumAll $ reward :: Float
+
+runAlgorithm episode iteration !agent envUrl _ !buffer obs total = do
+
+    when (verbose && iteration `elem` [0,10 .. numIterations]) do
+        putStrLn $ "Episode " ++ show episode ++ ", Iteration " ++ show iteration
+
+    (memories', !obs', !reward) <- evaluatePolicy episode iteration numSteps 
+                                                 agent envUrl obs buffer total
+
+    let !reward' = T.cat (T.Dim 1) [total, reward]
+        !buffer' = bufferPush' bufferSize buffer memories'
+
+    !agent' <- if bufferLength buffer' < batchSize 
+                  then pure agent
+                  else updatePolicy episode iteration agent buffer' numEpochs
+    
+    runAlgorithm episode iteration' agent' envUrl done' buffer' obs' reward'
+  where
+    done'      = iteration >= numIterations
+    iteration' = iteration + 1
+
+-- | Train Twin Delayed Deep Deterministic Policy Gradient Agent on Environment
+train :: Int -> Int -> HymURL -> IO Agent
+train obsDim actDim envUrl = do
+    remoteLogPath envUrl >>= setupLogging 
+
+    !agent <- makeAgent obsDim actDim >>= foldLoop' numEpisodes
+        (\agent' episode -> do
+            obs' <- toFloatGPU <$> resetPool envUrl
+            keys <- infoPool envUrl
+
+            let !obs    = processGace obs' keys
+                !buffer = makeBuffer
+                !reward = emptyTensor
+
+            runAlgorithm episode 0 agent' envUrl False buffer obs reward)
+    saveAgent agent ptPath
+    pure agent
+  where 
+      ptPath = "./models/" ++ algorithm
+
+-- | Play Environment with Twin Delayed Deep Deterministic Policy Gradient Agent
+--play :: Agent -> HymURL -> IO Agent
---
+++ src/TD3/Defaults.hs
@@ -0,0 +1,114 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module TD3.Defaults where
+
+import Lib
+import qualified Torch as T
+
+------------------------------------------------------------------------------
+--  General Default Settings
+------------------------------------------------------------------------------
+
+-- | Algorithm ID
+algorithm :: String
+algorithm     = "td3"
+-- | Print verbose debug output
+verbose :: Bool
+verbose       = True
+-- | Number of episodes to play
+numEpisodes :: Int
+numEpisodes   = 666
+-- | Horizon T
+numIterations :: Int
+numIterations = 150
+-- | Number of Steps to take with policy
+numSteps :: Int
+numSteps = 1
+-- | Number of epochs to train
+numEpochs :: Int
+numEpochs = 1
+-- | Early stop criterion
+earlyStop :: Float
+earlyStop     = -500.0
+-- | Mini batch of N transistions
+batchSize :: Int
+batchSize     = 100
+-- | Random seed for reproducability
+rngSeed :: Int
+rngSeed       = 666
+
+------------------------------------------------------------------------------
+--  ACE Environment Settings
+------------------------------------------------------------------------------
+           
+-- | ACE Identifier of the Environment
+aceId :: String
+aceId       = "op2"
+-- |  PDK/Technology backend of the ACE Environment
+aceBackend :: String
+aceBackend = "xh035"
+-- | ACE Environment variant
+aceVariant :: Int
+aceVariant  = 0
+-- | Number of Environments
+numEnvs :: Int
+numEnvs    = 20
+
+------------------------------------------------------------------------------
+--  TD3 Algorithm Hyper Parameters
+------------------------------------------------------------------------------
+
+-- | Policy and Target Critic Update Delay
+d :: Int
+d           = 2
+-- | Noise clipping
+c :: Float
+c           = 0.5
+-- | Discount Factor
+γ :: T.Tensor
+γ           = toTensor (0.99 :: Float)
+-- | Avantage Factor
+τ :: T.Tensor
+τ           = toTensor (1.0e-2 :: Float)
+-- | Sampling Noise as Tensor
+σ :: T.Tensor
+σ           = toTensor (0.2 :: Float)
+-- | Decay Period
+decayPeriod :: Int
+decayPeriod = round (1.0e5 :: Float)
+-- | Noise Clipping Minimum
+σMin :: Float
+σMin        = 1
+-- | Noise Clipping Maximuxm
+σMax :: Float
+σMax        = 1
+
+------------------------------------------------------------------------------
+-- Neural Network Parameter Settings
+------------------------------------------------------------------------------
+
+-- | Initial weights
+wInit :: Float
+wInit = 3.0e-3
+-- | Learning Rate
+ηθ :: T.Tensor
+ηθ     = toTensor (1.0e-4 :: Float)
+-- | Learning Rate
+ηφ :: T.Tensor
+ηφ     = toTensor (1.0e-4 :: Float)
+-- | Optimizer Betas
+β1   :: Float
+β1    = 0.9
+β2   :: Float
+β2    = 0.99
+
+------------------------------------------------------------------------------
+--  Memory / Replay Buffer Settings
+------------------------------------------------------------------------------
+
+-- | Replay Buffer Size
+bufferSize :: Int
+bufferSize    = round (1.0e7 :: Float)
+-- | Initial sample collecting period
+warmupPeriode :: Int
+warmupPeriode = 50
